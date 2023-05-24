71a67d6484ae3b8d44c42e410d839ad455366574
Arthur <zoz133722i@gmail.com>
2023-05-24 19:07:37 +0500

    lesson4

--- app.js
+++ app.js
@@ -1,22 +1,63 @@
-// Операторы присваивания
-let age = 10 + 10;
-age += 2; // age = age + 2
-age -= 3; // age = age - 3
-age *= 2; // age = age * 2
-age /= 2; // age = age / 2
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
 
-age++; // age = age + 1
-age--; // age = age - 1
+// 100 - (12) 10 > (10) 90 - (12) 5
 
-console.log(age);
+const a = (25 + 5) / 3;
+console.log(a);
 
-// Операторы сравнения
+let b;
+let c;
+c = b = 100;
+console.log(c);
+console.log(b);
 
-const denis = 35;
-console.log(age > denis);
-console.log(age >= denis);
-console.log(age < denis);
-console.log(age <= denis);
-console.log(age == denis);
+const number = 18; // Число
+const surName = "Петров"; // Строка
+const bulevo = true; // Булево значение
+const hz = undefined; // На задано значение
+let dat; // На задано значение
+let pusto = null; // Пустое значение
+const admin = Symbol("Admin"); // Уникальное неизменное значение
+const big = BigInt(999999999); // Работа с большими числами
 
-//lesson3
+let d = 5;
+let e = 5.6;
+console.log(typeof d);
+d = "list";
+console.log(typeof d);
+console.log(typeof bulevo);
+console.log(typeof hz);
+console.log(typeof pusto);
+
+const a1 = "a";
+const b1 = "b";
+const c1 = "c";
+const c1b1a1 = a1 + b1 + c1;
+console.log(c1b1a1);
+
+const a1b1c1 = `${a1} ${b1} ${c1}`; // const a1b1c1 = a1 + ' ' + b1 + ' ' + c1
+console.log(a1b1c1);
+
+const abc = `${a1}
+${b1}
+${a1b1c1}`;
+console.log(abc);
+
+const age = "25";
+console.log(Boolean(age) + 5);
+console.log(age - 10);
+const imya = "Denis";
+console.log(Number(imya) + 5);
+console.log(typeof NaN);
+const numbe = String(4) + 6;
+console.log(typeof numbe);
+console.log(Number("hei"));
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('')))
+
+//lesson4
