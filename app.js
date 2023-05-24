1d4b0d2aa1d324dee4ad11309616a8df5211fba0
Arthur <zoz133722i@gmail.com>
2023-05-24 14:21:48 +0500

    lesson2

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const width = 2;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const volume = 2 ** 3;
+console.log(volume);
+
+const street = "Pushkin";
+const houseName = "Kolotushkin";
+console.log(street + ", " + houseName + " " + 5); // lesson2
