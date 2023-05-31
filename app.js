1537314f3f6ea5b53598169a4f8fed17fe5c4d03
Arthur <zoz133722i@gmail.com>
2023-05-31 22:00:40 +0500

    lesson17

--- app.js
+++ app.js
@@ -1,104 +1,37 @@
 'use strict';
 
-const bmw = {
-  band: 'bmw',
-  model: 'm3',
-  year: 2023,
-  damages: [],
-  addDamage(part, degree) {
-    console.log(
-      `The car: ${this.band} ${this.model} ${this.year} added: - damage: ${part}, degree of damage: ${degree}`
-    );
-    this.damages.push({ part, degree });
+function submitForm() {
+  const input = document.querySelector('.inputs').value;
+  if (!input) {
+    return;
   }
-};
-
-bmw.addDamage('part1', 2);
-
-const toyota = {
-  band: 'toyota',
-  model: 'corolla',
-  year: 2023,
-  damages: []
-};
-
-toyota.addDamage = bmw.addDamage;
-
-toyota.addDamage('part2', 4);
-
-const addDamageFunc = bmw.addDamage;
-
-addDamageFunc.call(toyota, 'part3', 10);
-
-addDamageFunc.apply(bmw, ['part4', 5]);
-
-const carManipulation = {
-  addDamage(part, degree) {
-    console.log(`Добавить повреждение на ${this.band} ${this.model}`);
-    this.damages.push({ part, degree });
-  }
-};
-
-const addDamageToyota = carManipulation.addDamage.bind(toyota, 'part10');
-
-addDamageToyota(3);
-console.log(toyota);
-addDamageToyota(3);
-
-const user = {
-  password: '1233213'
-};
-
-function removePassword(reset) {
-  if (reset) {
-    this.password = undefined;
-  } else this.password = '1';
+  document.querySelector('.panel').innerText = input;
+  document.querySelector('.inputs').value = '';
+  document.querySelector('.notification').getAttribute('class');
+  document.querySelector('.notification').setAttribute('atribute', 'dasds');
 }
 
-const fRemovePassword = removePassword.bind(user);
-fRemovePassword();
-console.log(user.password);
-
-(function () {
-  console.log('Start');
-})();
-
-function changeBalance() {
-  let balance = 0;
-  return function (sum) {
-    balance += sum;
-    console.log(balance);
-  };
-}
-
-const change = changeBalance();
-change(100);
-change(100);
-
-console.dir(change);
-
-const userInfo = {
-  balance: 0,
-  operation: 0,
-  increse(sum) {
-    this.balance += sum;
-    this.operation++;
+function inputChange(e) {
+  if (e.code === 'Enter') {
+    submitForm();
   }
-};
-
-function addUser() {
-  const addUser = {
-    ...userInfo
-  };
-  return function () {
-    return addUser;
-  };
 }
+console.log(document.querySelectorAll('.one > span')[0].innerText);
+console.log(document.querySelectorAll('.one > span')[1].innerText);
+console.log(document.getElementById('two').innerText);
+console.log(document.querySelector('[user-id="9"]').innerText);
 
-const user1 = addUser();
-console.log(user1().increse(150));
-console.log(user1());
+const panelText = 'Panel';
+const panelClass = 'button';
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+newElement.innerHTML = `<button class = "${panelClass}">${panelText}</button>`;
+document.querySelector('.test').appendChild(newElement);
 
-const user2 = addUser();
-console.log(user2().increse(350));
-console.log(user2());
+localStorage.setItem('token', 'dasdas');
+localStorage.setItem('token1', 312);
+localStorage.setItem('token2', true);
+
+console.log(localStorage.getItem('token1'));
+localStorage.removeItem('token');
